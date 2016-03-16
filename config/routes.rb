879afd530b99b0320b74ac4269fece450df8cf02
Brendan Magee <bmagee@gmail.com>
2016-03-16 17:11:44 -0400

    fixed students controller, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "houses#index"
-  resources :house, only: [:index, :show]
+  resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
 end
