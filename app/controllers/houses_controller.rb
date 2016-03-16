734a0973172662d37ca9aa4acec4e687526b107f
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-16 15:19:59 -0400

    students links show photos

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,4 +8,17 @@ class HousesController < ApplicationController
     @house = House.find(params[:id])
   end
 
+  def new
+    @house = House.new
+  end
+  #
+  # def create
+  #   @house = House.create!(house_params)
+  #   redirect_to house_path(@house)
+  # end
+  #
+  # private
+  # def house_params
+  #   params.require(:house).permit(:name, :img_url)
+  # end
 end
