1578ac7713fe21e9040a49ec1c1515ca132b7a76
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-16 11:18:21 -0400

    application index loads

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,39 @@
-class HouseController < ApplicationController
+class HousesController < ApplicationController
 
+#index
   def index
-    @houses = Houses.all
+    @house = House.new
+    @houses = House.all
   end
 
+#show
   def show
-    @houses = Houses.all
+    @houses = House.all
   end
+
+#new
+def new
+  @house = House.new
+end
+
+
+#create
+def create
+  @house = House.create!(house_params)
+
+end
+
+
+#edit
+
+#update
+
+#delete
+
+private
+
+def house_params
+  params.require(:house).permit(:name, :image_url)
+end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,51 @@
 class StudentsController < ApplicationController
 
+#index
   def index
-    @student = Student.all
+    @students = Student.all
+    @student = Student.find(params[:id])
   end
 
+#show
   def show
-    @student = student.find(:id)
+    @student = Student.find(params[:id])
+    redirect_to house_student_path(@student)
   end
+
+#new
+  def new
+    @house = House.find(params[:id])
+    @student = @house.students.new
+  end
+
+#create
+  def create
+    @house = House.find(params[:id])
+    @student = @house.students.create!(student_params)
+  end
+
+#edit
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+
+#update
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+  end
+
+  #delete
+  def delete
+    @student = Student.find(params[:id])
+    @student.destroy
+  end
+
+private
+
+def student_params
+  params.require(:student).permit(:name, :img_url, :house_id)
+end
 
 end
