0b84409c04088522a15ab25b3577eb98465dae05
Patrick Moran <patrick_moran@mac.com>
2016-03-16 11:14:39 -0400

    got enroll to work

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.order("RANDOM()").limit(1)[0]
+    @student = @house.students.create!(student_params)
+    redirect_to student_url(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
   root "houses#index"
   resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :show]
-
+  resources :students, only: [:index, :show, :new, :create]
 end
