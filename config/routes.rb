c24215c2fca7b154502c24c58027879e1367f0c6
Keith Prifte <kprift@gmail.com>
2016-03-16 18:53:25 -0400

    validates and fixes HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Hogwarts::Application.routes.draw do
 
   root "houses#index"
-  resources :houses, only: [:index, :show] 
-  resources :students, only: [:index, :show]
+  resources :houses, only: [:index, :show]
+  resources :students, only: [:index, :show, :new]
 
 
 end
