071cdc780f2e4a0168f32cf5df0594c0b4c434d5
Toni Langley <chibia2@gmail.com>
2016-03-16 18:17:27 -0400

    add create/sorting hat/student params methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,8 +8,22 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-def new
-  @student = Student.new
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create!(student_params)
+    redirect_to students_path
+  end
+
+def sorting_hat
+  @spec = House.all.sample.id
 end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url).merge!({house_id: sorting_hat})
+  end
 
 end
