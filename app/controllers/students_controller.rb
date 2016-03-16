8382351f96b135b75b11faa7690753e3c73da333
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-16 13:24:26 -0400

    Can enroll students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,29 @@
-class StudentsController < ActionController
+class StudentsController < ApplicationController
 
   def index
-    @student = Student.all
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+    @house_id = House.all.sample.id
+  end
+
+  def create
+    @student = Student.create!(student_params)
+    @house_id = House.all.sample.id
+
+    redirect_to student_url(@student)
   end
 
   def show
-    @student = student.find(:id)
+    @student = Student.find(params[:id])
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Hogwarts::Application.routes.draw do
 
-  root "houses#index"
-  resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  root to: 'houses#index'
+  resources :houses
+  resources :students
 
 end
