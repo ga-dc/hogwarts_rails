3f5aedbd06b51cb0a51e3b50c71ab0a4829083df
Charles <charlesthwang@gmail.com>
2016-03-16 12:38:13 -0400

    validated application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,21 @@ Hogwarts::Application.routes.draw do
 
   resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
-
 end
+
+# Prefix Verb URI Pattern             Controller#Action
+#
+#     root GET  /                       houses#index
+#   houses GET  /houses(.:format)       houses#index
+#    house GET  /houses/:id(.:format)   houses#show
+# students GET  /students(.:format)     students#index
+#  student GET  /students/:id(.:format) students#show
+
+
+# Prefix Verb URI Pattern                              Controller#Action
+#
+#           root GET  /                                        houses#index
+# house_students GET  /houses/:house_id/students(.:format)     students#index
+#  house_student GET  /houses/:house_id/students/:id(.:format) students#show
+#         houses GET  /houses(.:format)                        houses#index
+#          house GET  /houses/:id(.:format)                    houses#show
