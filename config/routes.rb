dfde603f03e5f5b5b991ec1e45537d80f4c7b90d
Melissa Miller <lis.d.miller@gmail.com>
2016-03-16 19:50:53 -0400

    Fixes show pages for houses
    RARRRRRRRRRR!!!! MELISSA SMASH PLURAL VS SINGULAR!!!!!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "houses#index"
-  resource :houses, only: [:index, :show]
+  resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
 end
