33572846fb75780503422b4277eb71fa86e0f736
Woo Yun <hh747hh@gmail.com>
2016-03-16 16:21:14 -0400

    naver too late to commit...showing header...

--- db/migrate/20131030184440_create_students.rb
+++ db/migrate/20131030184440_create_students.rb
@@ -1,10 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :name, null: false
+      t.string :name
       t.string :img_url
-      t.references :house, null: false, index:true
-
+      t.references :house, null: false, index: true
       t.timestamps
     end
   end
