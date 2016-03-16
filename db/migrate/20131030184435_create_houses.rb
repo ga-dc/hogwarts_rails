6a2f4680f570b2ad7432f22c432900f746df1a62
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-16 12:33:30 -0400

    err in migration and models folders

--- db/migrate/20131030184435_create_houses.rb
+++ db/migrate/20131030184435_create_houses.rb
@@ -1,9 +1,8 @@
 class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
-      f.string :name, null: false
+      f.string :name
       f.string :img_url
-
       f.timestamps
     end
   end
