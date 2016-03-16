fa4af26c2ef3064f79e0700ec0d24e0262c168aa
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-16 12:36:02 -0400

    replaced f.string etc with t as in tablerow in migration

--- db/migrate/20131030184435_create_houses.rb
+++ db/migrate/20131030184435_create_houses.rb
@@ -1,9 +1,9 @@
 class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
-      f.string :name
-      f.string :img_url
-      f.timestamps
+      t.string :name
+      t.string :img_url
+      t.timestamps
     end
   end
 end
