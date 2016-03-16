a531a192c62d9c3802e833ed3cad90be58feddb7
Keith Prifte <kprift@gmail.com>
2016-03-16 12:18:08 -0400

    fixes view, working on show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Hogwarts::Application.routes.draw do
 
   root "houses#index"
-  resource :houses, only: [:index, :show]
+  resources :houses, only: [:index, :show] 
   resources :students, only: [:index, :show]
+
 
 end
