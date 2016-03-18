f2371608d0f9628a5867940f0c5948b4356a746f
Ryan Krolick <rkrolick@gmail.com>
2016-03-17 23:11:41 -0400

    App fixed, Sorting Hat functionality added.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,8 +8,21 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.create!(student_params)
+    @student.house = House.random_house
+    @student.save!
+    redirect_to student_path(@student)
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
+  end
+
 
 end
