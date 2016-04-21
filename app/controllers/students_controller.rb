3035191236408099c081e0ba65ec58981cded8e1
Hewan Abreha <henebys@gmail.com>
2016-04-21 14:44:33 -0400

    fixes errors, resources

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,26 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+    @houses= House.all
+  end
+  
+  def create
+    @student = Student.create(student_params)
+    redirect_to @student
+  end
+
+  private
+ def student_params
+   params.require(:student).permit(:name, :img_url, :house_id)
+ end
+
+end
