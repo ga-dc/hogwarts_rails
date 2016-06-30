9b8986a68878a62838f28adc52dc1a0fedcd4cb4
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-30 15:13:47 -0400

    enroll working, not yet random ordering of assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,11 +5,22 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(:id)
+    @student = Student.find(params[:id])
   end
 
   def new
     @student = Student.new
   end
+
+  def create
+    @student = Student.create!(student_params)
+    redirect_to students_path
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house_id)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Hogwarts::Application.routes.draw do
   root to: 'houses#index'
   resources :houses, only: [:index, :show]
 
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show, :new, :create]
 end
