641e1f6a59f9335d36181c25f9d982b7322e870b
Andrew Young <prince.of.utah@gmail.com>
2016-06-30 12:41:44 -0400

    final commit - got it all to work and look decent

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,39 +4,8 @@ class HousesController < ApplicationController
     @houses = House.all
   end
 
-  def new
-    @house = House.new
-  end
-
-  # create
-  def create
-    @house = House.create!(house_params)
-    redirect_to (house_path(@house))
-  end
-
   def show
     @house = House.find(params[:id])
   end
-  def edit
-    @house = House.find(params[:id])
-  end
-
-
-  # update
-  def update
-    @house = House.find(params[:id])
-    @house.update(house_params)
-    redirect_to house_path(@house)
-  end
-
-  # destroy
-  def destroy
-    @house = House.find(params[:id])
-    @house.destroy
-    redirect_to houses_path
-  end
-  private
-  def house_params
-    params.require(:house).permit(:name)
-  end
+  
 end
