ee353abaaedbd472216d24ddb56365f07b05f19e
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-30 13:05:03 -0400

    Bonus Completed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,11 @@
 class Student < ActiveRecord::Base
   belongs_to :house
+
+  def self.sorting_hat student
+    prng = Random.new
+    house_id = prng.rand(1..4)
+    student.house = House.find(house_id)
+    return student
+
+  end
 end
