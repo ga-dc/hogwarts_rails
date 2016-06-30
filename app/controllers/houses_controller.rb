969fd4c6bb4b4e738ed3904e14109f09103788e0
Andrew Young <prince.of.utah@gmail.com>
2016-06-30 10:27:15 -0400

    initial commit

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,42 @@
 class HouseController < ApplicationController
 
   def index
-    @houses = Houses.all
+    @house = House.all
+  end
+
+  def new
+    @house = House.new
+  end
+
+  # create
+  def create
+    @house = House.create!(house_params)
+    redirect_to (house_path(@house))
   end
 
   def show
-    @houses = Houses.all
+    @house = House.find(params[:id])
+  end
+  def edit
+    @house = House.find(params[:id])
   end
 
+
+  # update
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
+  # destroy
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path
+  end
+  private
+  def house_params
+    params.require(:house).permit(:name)
+  end
 end
