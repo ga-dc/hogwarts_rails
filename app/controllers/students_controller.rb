920a3923b126179387fb9352155e7f7318c68f74
Mara Sanders <sandersmara@gmail.com>
2016-06-30 14:37:17 -0400

    Added Delete Student Feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,6 +20,13 @@ def create
   redirect_to @student
 end
 
+def destroy
+  @student = Student.find(params[:id])
+  @student.destroy
+  redirect_to students_path
+end
+
+
 private
 def student_params
   params.require(:student).permit(:name, :img_url)
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,5 @@ class Student < ActiveRecord::Base
   belongs_to :house
   def assign
     self.house = House.all.sample
-  end
+  end 
 end
