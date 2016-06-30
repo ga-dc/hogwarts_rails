bab5440ab369179e9e341ddc1059cbce72b72e84
Rebekah Milford <rmilford86@gmail.com>
2016-06-30 12:41:38 -0400

    got everything functional

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,39 +4,42 @@ class HousesController < ApplicationController
    @houses = House.all
  end
 
- def new
-   @house = House.new
- end
-
- # create
- def create
-   @house = House.create!(house_params)
-   redirect_to (house_path(@house))
- end
-
  def show
    @house = House.find(params[:id])
  end
- def edit
-   @house = house.find(params[:id])
  end
+ #
+ # def new
+ #   @house = House.new
+ # end
+
+ # # create
+ # def create
+ #   @house = House.create!(house_params)
+ #   redirect_to (house_path(@house))
+ # end
+
+
+ # def edit
+ #   @house = house.find(params[:id])
+ # end
 
 
  # update
- def update
-   @house = House.find(params[:id])
-   @house.update(house_params)
-   redirect_to house_path(@house)
- end
-
- # destroy
- def destroy
-   @house = House.find(params[:id])
-   @house.destroy
-   redirect_to houses_path
- end
- private
- def house_params
-   params.require(:house).permit(:name)
- end
-end
+#  def update
+#    @house = House.find(params[:id])
+#    @house.update(house_params)
+#    redirect_to house_path(@house)
+#  end
+#
+#  # destroy
+#  def destroy
+#    @house = House.find(params[:id])
+#    @house.destroy
+#    redirect_to houses_path
+#  end
+#  private
+#  def house_params
+#    params.require(:house).permit(:name)
+#  end
+# end
