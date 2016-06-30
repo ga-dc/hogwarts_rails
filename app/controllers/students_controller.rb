b2ea9fcf6526119d5ac713442c44d5af934831f2
Mara Sanders <sandersmara@gmail.com>
2016-06-30 13:47:22 -0400

    Hogwarts Functional

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,28 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+  # new
+def new
+  @student = Student.new
+end
+
+# create
+def create
+  @student = Student.new(student_params)
+  @student.assign
+  @student.save
+  redirect_to @student
+end
+
+private
+def student_params
+  params.require(:student).permit(:name, :img_url)
+end
+
+end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
-def Student < ActiveRecord::Base
-  belongs_to :houses
+class Student < ActiveRecord::Base
+  belongs_to :house
+  def assign
+    self.house = House.all.sample
+  end
 end
