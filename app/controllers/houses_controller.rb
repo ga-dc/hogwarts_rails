bede8928e20121a8a88e2a1be6fcf938c4fe86bd
Rebekah Milford <rmilford86@gmail.com>
2016-06-30 11:33:30 -0400

    kind of working

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,42 @@
-class HouseController < ApplicationController
+class HousesController < ApplicationController
 
-  def index
-    @houses = Houses.all
-  end
+ def index
+   @houses = House.all
+ end
 
-  def show
-    @houses = Houses.all
-  end
+ def new
+   @house = House.new
+ end
 
+ # create
+ def create
+   @house = House.create!(house_params)
+   redirect_to (house_path(@house))
+ end
+
+ def show
+   @house = House.find(params[:id])
+ end
+ def edit
+   @house = house.find(params[:id])
+ end
+
+
+ # update
+ def update
+   @house = House.find(params[:id])
+   @house.update(house_params)
+   redirect_to house_path(@house)
+ end
+
+ # destroy
+ def destroy
+   @house = House.find(params[:id])
+   @house.destroy
+   redirect_to houses_path
+ end
+ private
+ def house_params
+   params.require(:house).permit(:name)
+ end
 end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,63 @@
+class StudentsController < ApplicationController
+ # index
+ def index
+   @students = Student.all
+ end
+
+ # new
+ def new
+   @house = House.find(params[:house_id])
+   @student = Student.new
+ end
+
+ # create
+ def create
+   @house = House.find(params[:house_id])
+   @student = Student.create!(student_params.merge(house: @house))
+   redirect_to house_student_path(@house, @student)
+ end
+
+ #show
+ def show
+   @student = Student.find(params[:id])
+ end
+
+ # edit
+ def edit
+   @student = Student.find(params[:id])
+ end
+
+ # update
+ def update
+   @student = Student.find(params[:id])
+   @house = House.find(params[:house_id])
+   @student.update(student_params.merge(house: @house))
+   redirect_to house_student_path(@student.house, @student)
+ end
+
+ def add_favorite
+   @student = Student.find(params[:id])
+   @student.favorites.create(user: current_user)
+   @house = @student.house
+   redirect_to @house
+ end
+
+ def remove_favorite
+   @student = Student.find(params[:id])
+   @student.favorites.where(user: current_user).destroy_all
+   @house = @student.house
+   redirect_to @house
+ end
+
+ # destroy
+ def destroy
+   @student = student.find(params[:id])
+   @student.destroy
+   redirect_to students_path
+ end
+
+ private
+ def student_params
+   params.require(:student).permit(:name, :preview_url, :house_id)
+ end
+end
