a8b786f66642c55ee6a1859bbda116ba87b512c4
Kevin Golden <kwg@cs.net>
2016-07-01 09:46:51 -0400

    Fixed folder problem

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,9 @@ class Student < ActiveRecord::Base
   belongs_to :house
 end
 
-def house_sort
-  houses = ["1", "2", "3", "4"]
-  x = houses.shuffle
-  y = x.pop
-
-end
+# def house_sort
+#   houses = ["1", "2", "3", "4"]
+#   x = houses.shuffle
+#   y = x.pop
+#
+# end
