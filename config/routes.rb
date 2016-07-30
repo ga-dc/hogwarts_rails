b744942108290fc17d35382b84d03b00dcdf8ed0
Marc Fournier <marc.fournier91@gmail.com>
2016-07-30 12:19:40 -0400

    Gave application layout a closing title tag so the page could be rendered. Changed the eroll link to a new student path. Added the new action to students resources. Changed 'house' resource to 'houses' so that a houses_path would exist.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Hogwarts::Application.routes.draw do
 
   root "houses#index"
-  resource :house, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  resources :houses, only: [:index, :show]
+  resources :students, only: [:index, :show, :new]
 
 end
