c9f8fe570074f6fc9742457631246aff641087dd
Marc Fournier <marc.fournier91@gmail.com>
2016-07-30 16:41:19 -0400

    Adds ability to create new students by writing their house number in(1 through 4).

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to @student
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house_id)
+  end
+
 end
