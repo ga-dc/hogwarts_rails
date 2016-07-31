f53f6a5d4606b67b37c284224ee46fd572997901
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-30 21:37:17 -0400

    finished assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,18 @@ class StudentsController < ApplicationController
     @student = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
   def show
     @student = Student.find(params[:id])
   end
 
+
+  def create
+    @student = Student.create(params[:id])
+    redirect_to @student
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
+Hogwarts::Application.routes.draw do
 
   root "houses#index"
-  resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :show]
+
+  resources :houses
+  resources :students
 
 end
