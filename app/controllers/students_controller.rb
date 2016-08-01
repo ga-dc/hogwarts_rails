ad647a4b8d92a792f28fa5b14da7c26497babf31
Megan Coyle <meg@mcoyle.com>
2016-08-01 09:53:44 -0400

    Get student show page to show properly

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,8 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+    find_id = Student.find(params[:id]).house_id
+    @house = House.find(find_id)
   end
 
 end
