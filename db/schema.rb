e922453e4f180a14e227acc8b5aadced45f97b22
pamelaolney <pjolney12@gmail.com>
2016-08-01 16:35:58 -0400

    can now view houses!

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord: :Schema.define(version: 20131030184440) do
+ActiveRecord::Schema.define(version: 20131030184440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "houses", force: :cascade do |t|
-    t.string   "name",
+    t.string   "name"
     t.string   "img_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at", null: false
