a32ebe921ae90eedee36c6d5bbeb0546bee9a35b
Latoya Watson <latoyamwatson@gmail.com>
2016-10-10 16:53:23 -0400

    fixing errors, taking names

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    @student = Student.all
+    @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
   end
 
 end
