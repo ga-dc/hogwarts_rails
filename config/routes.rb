eac32c386920b1978ca1981140be9b54d1fa64e7
Nick Oki <nick.oki@live.com>
2016-10-10 16:14:05 -0400

    New Student link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root "house#index"
   resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show, :new]
 
 end
