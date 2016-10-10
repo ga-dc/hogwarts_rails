557892fc5e7c2f2c983ed7cb6103e08530732317
Andrew Whitley <awhitley1233@gmail.com>
2016-10-10 15:55:42 -0400

    finished all

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,26 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.house_id = rand(1...4)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
+  end
+end
