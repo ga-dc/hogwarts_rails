6bce6ba3493a928a996cece8f33c3a2dcb404c0b
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-10 19:58:13 -0400

    create enroll form

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student =  Student.new
+  end
+
+  def create
+    @student = Student.create(params_student)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Hogwarts::Application.routes.draw do
 
-  root "house#index"
-  resource :house, only: [:index, :show]
-  resources :students, only: [:index, :show]
-
+resources :houses, :students
 end
