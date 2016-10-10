3b9a09e9d2bdf3e07740992c63f2ec98460dbb1b
Michael Lai <mike24567@gmail.com>
2016-10-10 15:18:40 -0400

    Finally able to load page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Hogwarts::Application.routes.draw do
 
-  root "house#index"
-  resource :house, only: [:index, :show]
-  resources :students, only: [:index, :show]
+resources :houses, :students
 
 end
