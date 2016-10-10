3ec45bec2b55e8c19527bb53a4ebdfd8549269ef
Khoi Le <khoileee89@gmail.com>
2016-10-10 15:41:10 -0400

    seeded and tables created

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,8 @@
-class HouseController < ApplicationController
-
+class HousesController < ApplicationController
   def index
-    @houses = Houses.all
+    @house = House.all
   end
-
   def show
-    @houses = Houses.all
+    @houses = House.all
   end
-
 end
