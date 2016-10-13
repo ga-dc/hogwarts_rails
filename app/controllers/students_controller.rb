e4d8b3e4107f8c447ec07153d54a2f3d6d4f816d
Khoi Le <khoileee89@gmail.com>
2016-10-13 08:22:43 -0400

    mvp

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
   def new
-    
+    # @house = House.find(params[:house_id])
+  @student = Student.new
   end
+  def create
+    @student = Student.new(student_params)
+    @student.house_id = rand(33..36)
+    @student.save
+    redirect_to houses_path
+  end
+private
+def student_params
+  params.require(:student).permit(:name, :img_url, :house_id)
 
+end
 end
