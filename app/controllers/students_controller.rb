f2b4726f595b625ae1d22b202b5aad7244e6252d
Jonathan Reiser <jonathanreiser@Jonathans-MacBook-Pro-2.local>
2016-11-08 13:39:59 -0500

    homework

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,21 @@
 class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+
+    redirect_to students_path
+    end
+
+  def create
+    new_student = Student.new(student_params)
+    new_student.house_id = House.test
+    new_student.save
+    redirect_to students_path
+  end
 
   def index
     @students = Student.all
@@ -6,6 +23,10 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+  end
+  private
+  def student_params
+    params.require(:student).permit(:name, :house_id)
   end
 
 end
