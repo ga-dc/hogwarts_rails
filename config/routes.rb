bfb9973f0b086b7aa2fef41bfa165616f2899c09
Diana B. Gobar <dianabgobar@gmail.com>
2016-11-09 14:20:13 -0500

    fixed yield, now lists are viewable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Hogwarts::Application.routes.draw do
   get "houses" => "houses#index"
   get "houses/:id" => "houses#show"
 
-  resource :houses #, only: [:index, :show]
-  resources :students #, only: [:index, :show]
+  resource :houses , only: [:index, :show]
+  resources :students , only: [:index, :show]
 
 end
