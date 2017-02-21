5b2d65682d6fc32545a7250f7a8b7b8c28a858ea
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-21 16:41:23 -0500

    saving progress towards the bonus section

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,35 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @new_student = Student.new
+    @new_student.house_id = (1..4).to_a.sample.to_i
     puts "*" * 60
     puts "*" * 60
-    puts params.inspect
+    puts @new_student
+    puts @new_student.house_id
     puts "*" * 60
     puts "*" * 60
   end
+
+  def create
+
+    @new_student = Student.create!(student_params)
+    redirect_to student_path(@student)
+  end
+
+  def sorting_hat
+    # house_id: 1
+    #@student.house_id = 1
+    # house = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"].sample
+    # params[:house_id] = house
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :img_url, :house)
+    end
 
 end
