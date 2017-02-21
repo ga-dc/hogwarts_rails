874202f714ebfcbc24ae36f8191b0ee400c4b785
Dominick Houston <dominick.houston14@gmail.com>
2017-02-21 18:38:12 -0500

    Complete

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,14 @@
-class HouseController < ApplicationController
+class HousesController < ApplicationController
 
   def index
-    @houses = Houses.all
+    @houses = House.all
   end
 
   def show
-    @houses = Houses.all
+    @houses = House.all
+
+    @house = House.find_by(params[:id])
+
   end
 
 end
