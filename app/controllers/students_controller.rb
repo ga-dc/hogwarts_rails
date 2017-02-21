c1edae86ba704abc016bf29223563bb17a377f45
Erica Norby <ericanorby@gmail.com>
2017-02-21 17:00:45 -0500

    added create student functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,21 @@ class StudentsController < ApplicationController
   end
 
   def new
-  
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.house = House.all.sample
+    @student.save
+
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :img_url)
   end
 
 end
