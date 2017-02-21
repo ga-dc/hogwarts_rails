d39f579103729356510f9a179de6459f6a31161c
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-02-21 18:55:13 -0500

    layout works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
-Hogwarts::Application.routes.draw do
+Rails.application.routes.draw do
 
-  root "houses#index"
+  root to: 'houses#index'
   resource :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
+#houses routes
+
+  get     '/houses',          to: 'houses#index'
+  get     '/houses/new',      to: 'houses#new'
+  post    '/houses',          to: 'houses#create'
+
+  get     '/houses/:id',      to: 'houses#show'
+  get     '/houses/:id/edit', to: 'houses#edit'
+  put     '/houses/:id',      to: 'houses#update'
+  delete  '/houses/:id',      to: 'houses#destroy'
+
+  # students routes
+  get     '/students',          to: 'students#index'
+  get     '/students/new',      to: 'students#new'
+  post    '/students',          to: 'students#create'
+
+  get     '/students/:id',      to: 'students#show'
+  get     '/students/:id/edit', to: 'students#edit'
+  put     '/students/:id',      to: 'students#update'
+  delete  '/students/:id',      to: 'students#destroy'
 end
