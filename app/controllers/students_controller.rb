4f99a98efe0ba880c975b0b5b91b30628fd7bc01
Hannah Schuster <hschuster9@gmail.com>
2017-02-21 17:55:54 -0500

    updated to view houses, students, and create student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,34 @@
 class StudentsController < ApplicationController
 
   def index
-    @house = House.find(params[:house_id])
-    # @student = Student.all
-    @student = @house.students.find(params[:id])
+    @student = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+
+  end
+
+  def create
+    @student = Student.create!(student_params)
+    redirect_to students_path
+  end
+
+  def destroy
+  @student = Student.find(params[:id])
+  @student.destroy
+  redirect_to students_path
+  end
+
+
+
+
+private
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Hogwarts::Application.routes.draw do
 
   root to:  "houses#index"
   resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :show, :new]
+  resources :students, only: [:index, :show, :new, :create, :destroy]
 
 end
