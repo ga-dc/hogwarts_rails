96d0b52ab638de84957132a7788d3e57357101cc
Nat Carter <n.goldman.carter@gmail.com>
2017-02-21 17:06:49 -0500

    new students can enroll now

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,19 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.all.sample
+    @student = Student.create!(student_params.merge(house: @house))
+    redirect_to student_path(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
+  end
 end
