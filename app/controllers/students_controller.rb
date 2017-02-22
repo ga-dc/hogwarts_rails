2f32c677aaac276cd5120a5f9b5c5825cb4f07ac
Fiona <h.natapra@gmail.com>
2017-02-22 08:56:46 -0500

    edit & update students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,38 @@
 class StudentsController < ApplicationController
 
   def index
-    @house = House.find(params[:house_id])
-    @students = @house.students
+    # @house = House.find(params[:house_id])
+    @students = Student.all
   end
 
   def show
-    @house = House.find(params[:house_id])
+    # @house = House.find(params[:house_id])
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.house= House.all.sample
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(song_params)
+    redirect_to student_path(@student)
+  end
+
+private
+def student_params
+  params.require(:student).permit(:name, :img_url)
+end
 end
