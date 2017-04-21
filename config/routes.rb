fa07f0092793e616b926e91a4e20e1c5d79a752f
Erin <erinedawson@gmail.com>
2017-04-21 14:09:52 -0400

    seed houses data

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
 
   root to: 'houses#index'
 
-  resource :houses, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  resource :houses do
+    resources :students
+  end
 
-  
-    get     '/houses',          to: 'houses#index'
-    get     '/houses/new',      to: 'houses#new'
-    post    '/houses',          to: 'houses#create'
-    get     '/houses/:id',      to: 'houses#show'
-
-
-    get     '/students',          to: 'students#index'
-    get     '/students/new',      to: 'students#new'
-    post    '/students',          to: 'students#create'
-    get     '/students/:id',      to: 'students#show'
+    # get     '/houses',          to: 'houses#index'
+    # get     '/houses/new',      to: 'houses#new'
+    # post    '/houses',          to: 'houses#create'
+    # get     '/houses/:id',      to: 'houses#show'
+    #
+    #
+    # get     '/students',          to: 'students#index'
+    # get     '/students/new',      to: 'students#new'
+    # post    '/students',          to: 'students#create'
+    # get     '/students/:id',      to: 'students#show'
 
 end
