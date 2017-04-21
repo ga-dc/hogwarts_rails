5d6e9f9bcfc301c0a7fb0888873570ce39a8555e
Erin <erinedawson@gmail.com>
2017-04-21 12:01:18 -0400

    add routes, fix title in layout, fix img in houses index

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,16 @@ Rails.application.routes.draw do
   resource :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
+  
+    get     '/houses',          to: 'houses#index'
+    get     '/houses/new',      to: 'houses#new'
+    post    '/houses',          to: 'houses#create'
+    get     '/houses/:id',      to: 'houses#show'
+
+
+    get     '/students',          to: 'students#index'
+    get     '/students/new',      to: 'students#new'
+    post    '/students',          to: 'students#create'
+    get     '/students/:id',      to: 'students#show'
+
 end
