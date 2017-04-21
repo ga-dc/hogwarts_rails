792b0ffcf154cadb0d4efaacb5f4280b4a8e6785
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-21 09:53:15 -0400

    second commits

--- app/controllers/StudentController.rb
+++ app/controllers/StudentController.rb
@@ -1,11 +1,11 @@
-class StudentsController < ActionController
+class StudentsController < ApplicationController
 
-  def index
-    @student = Student.all
-  end
+    def index
+        @students = Student.all
+    end
 
-  def show
-    @student = student.find(:id)
-  end
+    def show
+        @student = student.find(:id)
+    end
 
 end
