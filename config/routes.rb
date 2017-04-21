a67549327cf9e7178fc383d8393e9aa33d87286b
Whitney <whitney.purdum@gmail.com>
2017-04-21 14:26:01 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "houses#index"
   resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show, :new]
+  post "students" => "students#create"
 
 end
