6de88f1a5871b928cdc052095d8334b6cba6a636
Jeong Hoon Koo <junghoone17@gmail.com>
2017-04-21 12:30:31 -0400

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'houses#index'
+  root to: 'application#index'
 
     resources :houses do
       resources :students
     end
-    
+
   resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
