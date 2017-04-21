92e08b918f8ec0091256d476fd71b8adc96cfcdd
Caroline Chen <carolinecaroline@gmail.com>
2017-04-21 14:31:26 -0400

    fix bugs for show houses and show students

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  	@student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+
+    redirect_to "/students/#{@student.id}"
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url, :house)
+  end
+
+end
