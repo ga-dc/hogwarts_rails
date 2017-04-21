43287b3e19113406590bd48d11fdcf3a43e0981e
Jonathan Wright <jonwright.va@gmail.com>
2017-04-21 14:32:58 -0400

    started enroll student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,23 @@ class StudentsController < ApplicationController
     @house = House.find(@student.house_id)
   end
 
+  def new
+    @student = Student.new 
+
+  end
+
+  # create
+  def create
+
+    @student = @house_rand.students.create(student_params)
+    @house_rand = random
+    redirect_to student_path(@student)
+  end
+
+  private
+  def random
+  	house_num = (1..4).shuffle
+  	return [gryffindor, hufflepuff, ravenclaw, slytherin].at(house_num)
+  end
+
 end
