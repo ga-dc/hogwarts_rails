73c95022a9514035d840877fa4fb8626932fcb21
amlak befekadu <amlakgirma@yahoo.com>
2017-04-22 07:06:13 -0400

    Lab hogwarts_rails

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+   def new
+     @student = Student.new
+   end
+   def create
+     @student = Student.create
+   end
+end
