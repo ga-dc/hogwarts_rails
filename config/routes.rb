7e322df8b2c678436c0ecd0e2ee7db62d5c903c2
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-04-24 13:42:17 -0400

    new commits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Hogwarts::Application.routes.draw do
 
+  get 'houses/index'
+
   root "houses#index"
-  resource :houses, only: [:index, :show]
+  resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show]
 
 end
