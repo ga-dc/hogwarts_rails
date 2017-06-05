83bcdf460e30257552514ab1cc2e7d4fc0ebd5f3
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-05 14:25:36 -0400

    fixed link error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
   root to: "houses#index"
-  # resources :houses, only: [:index, :show] do
-  #   resources :students, only: [:index, :show]
-  # end
+
 
   resources :houses, only: [:index, :show]
   resources :students, only: [:index, :show, :new]
+
+  # resources :students, only: [:index, :show] do
+  #   resources :houses, only: [:index, :show]
+  # end
+
 
 
 end
