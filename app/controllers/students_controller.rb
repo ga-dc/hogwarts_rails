d31feb9c5ebee3a7c4572756389aec239cef31ef
Tim Chase <tchase4544@gmail.com>
2017-06-05 14:00:01 -0400

    students complete with Enroll feature. no more errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,11 +2,26 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+  end
 
+  def new
+  	@rand_no = rand(0..4)
+  	@student = Student.new
+  end
+
+  def create
+  	
+  	@student = Student.create!(student_params)
+
+  	redirect_to student_path(@student)
   end
 
   def show
     @student = Student.find(params[:id])
   end
 
+  private
+  	def student_params
+  		params.require(:student).permit(:name,:img_url,:house_id)
+  	end
 end
