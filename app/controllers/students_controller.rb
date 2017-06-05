6ec2f742a6f56b6a013de9df0d28c344f8fc99d2
Tim Chase <tchase4544@gmail.com>
2017-06-05 11:47:21 -0400

    house show pages fixed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    @student = Student.all
+    @students = Student.all
+
   end
 
   def show
-    @student = student.find(:id)
+    @student = Student.find(:id)
   end
 
 end
