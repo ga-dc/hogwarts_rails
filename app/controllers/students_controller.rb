ab67b38ab73870684aac101d3509c8bb1734ce43
Tarik Kazanci <tarikkzc@gmail.com>
2017-06-07 08:52:43 -0400

    hogwarts

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,26 @@
 class StudentsController < ApplicationController
 
   def index
-    @house = House.all
+    # @house = House.all
     @students = Student.all
   end
 
   def show
-    @house = House.all
+    # @house = House.all
     @student = Student.find(params[:id])
   end
+
+  # def new
+  #   @student = Student.new
+  # end
+  #
+  # def create
+  #   @student = Student.create(student_params)
+  # end
+  #
 
   private
      def student_params
        params.require(:student).permit(:name, :img_url)
      end
-
 end
