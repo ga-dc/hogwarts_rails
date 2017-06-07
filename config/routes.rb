ce035e2b686116a5208071010b5f548c4d0f8ed6
Bao Cong Pham <BaoPham92@yahoo.com>
2017-06-07 08:53:58 -0400

    Debbugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Hogwarts::Application.routes.draw do
+# removing the root line solved this problem or unintialized constants.
+  
+  root 'houses#index'
 
-  root "house#index"
   resource :house, only: [:index, :show]
   resources :students, only: [:index, :show]
 
