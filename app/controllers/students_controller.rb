7911bec126d8171d5dd37dab73b61303e772219f
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-31 12:15:28 -0400

    added new studnt page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,20 @@ class StudentsController < ApplicationController
     @house = @student.house
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.all.sample
+    @student = @house.students.create!(student_params)
+    redirect_to "/students/#{@student.id}"
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:img_url, :name)
+  end
+
+
 end
