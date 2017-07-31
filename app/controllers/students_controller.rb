450d5f74e5eedc7f8029b4d8e04fe45a264afb34
Kendall <adkins.kendall90@gmail.com>
2017-07-31 12:42:07 -0400

    bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,22 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.all.sample
+    @student = Student.new(student_params)
+    @student.house = @house
+    @student.save
+
+    redirect_to "/students/#{@student.id}"
+  end
+
+private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
+  end
+
 end
