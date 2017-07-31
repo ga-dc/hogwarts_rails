6619f3aa2e4d3f01b4912c1f8b58dd079482038c
Admin <Tgafvert@gmail.com>
2017-07-31 14:00:41 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,22 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(:id)
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.all.sample
+    @student = @house.students.create!(student_params)
+    redirect_to "/students/#{@student.id}"
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:img_url, :name)
   end
 
 end
