786cb862402b6487466bdf7cf19249335f4c066b
Charles Cox <cox.charles.w@gmail.com>
2017-07-31 11:59:04 -0400

    finish bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,23 @@ class StudentsController < ApplicationController
     @student = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @house = House.all.sample
+    @student = @house.student.create!(student_params)
+    redirect_to student_path(@student)
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :img_url)
   end
 
 end
