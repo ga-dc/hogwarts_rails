2f332a8af3eabc18bf6c36f9e7780b6b31aeb333
Perry Fustero <pjfust@gmail.com>
2017-07-31 11:52:49 -0400

    created enroll feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,24 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.house = House.all.sample
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :img_url, :house_id)
+    end
 
 end
