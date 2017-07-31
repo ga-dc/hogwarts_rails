307d942c9c3f0faf3291e7ad4488142f1d019a7e
Frew A Negash <zulufanke@yahoo.com>
2017-07-31 16:52:26 -0400

    Hogwarts lab summitted

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,20 @@
-class HouseController < ApplicationController
+class HousesController < ApplicationController
 
   def index
-    @houses = Houses.all
+    @houses = House.all
+
   end
 
   def show
-    @houses = Houses.all
+    @house = House.find(params[:id])
   end
 
+  def edit
+    @house = House.find(params[:id])
+
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to "/houses/#{@houses.id}"
 end
