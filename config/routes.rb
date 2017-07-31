35f10dc0e4c6482de5c5a790f8ddf7a05517add5
Mitche Guevara <mitche.guevara@gmail.com>
2017-07-31 12:47:08 -0400

    no bonus

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Hogwarts::Application.routes.draw do
 
-  root "house#index"
-  resource :house, only: [:index, :show]
-  resources :students, only: [:index, :show]
-
+  root to: "houses#index"
+  resources :houses, only: [:index, :show]
+  resources :students, only: [:index, :show, :new]
 end
+
+
+
+# get "hello/:name" => "greetings#hello"
+#   get "houses" => "artists#index"
+#   get "houses/new" => "artists#new"
+#   post "houses" => "artists#create"
+#   get "houses/:id" => "artists#show"
+#   get "houses/:id/edit" => "artists#edit"
+#   put "houses/:id" => "artists#update"
+#   delete "artists/:id" => "artists#destroy"
+#
+#   get "songs" => "songs#index"
+#   get "songs/new" => "songs#new"
+#   post "songs" => "songs#create"
+#   get "songs/:id" => "songs#show"
+#   get "songs/:id/edit" => "songs#edit"
+#   put "songs/:id" => "songs#update"
+#   delete "songs/:id" => "songs#destroy"
