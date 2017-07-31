815e6498287526df0cad57ac778d8fbea990aa36
ramiGA <ramisn3@gmail.com>
2017-07-31 14:34:40 -0400

    lab2

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'https://voldemortwashere.org'
+source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 
-gem 'rails', '4.2.5
+gem 'rails', '5.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
@@ -22,6 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rails_12factor', group: :production
 
 gem 'pry-rails'
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,21 +16,19 @@ ActiveRecord::Schema.define(version: 20131030184440) do
   enable_extension "plpgsql"
 
   create_table "houses", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "img_url",    null: false
+    t.string   "name"
+    t.string   "img_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "img_url",    null: false
+    t.string   "name"
+    t.string   "img_url"
     t.integer  "house_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["house_id"], name: "index_students_on_house_id", using: :btree
   end
 
-  add_index "students", ["house_id"], name: "index_students_on_house_id", using: :btree
-
-  add_foreign_key "students", "houses", name: "fk_students_houses"
 end
